2e77f2ba45280634dbd409fa716cc2e61fd8637f
sneakydonkey <82854985+sneakydonkey@users.noreply.github.com>
2021-05-29 02:40:31 -0700

    Create 0035_search_insert_position.go

---
+++ 0035_search_insert_position.go
@@ -0,0 +1,14 @@
+func searchInsert(nums []int, target int) int {
+    var left, right = -1, len(nums)
+    for right - left > 1 {
+        var mid = (left + right) / 2
+        if nums[mid] > target {
+            right = mid
+        } else if nums[mid] < target {
+            left = mid
+        } else {
+            return mid
+        }
+    }
+    return right
+}
